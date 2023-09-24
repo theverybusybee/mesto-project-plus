35b3af5a3a16deaa5d19b0f099d03c4da7f57244
theverybusybee <veronika.antonchik@gmail.com>
2023-09-24 12:37:28 +0300

    mongodb url changed

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
 
